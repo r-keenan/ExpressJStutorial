3b2fb5661785d8dc664b4bdf944a045418adf98a
Ross Keenan <ross.keenan@protonmail.com>
2021-06-21 14:13:59 -0500

    Added in Nodemon.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "/templates/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/templates/about.html"));
 });
 
 app.listen(port, () => {
