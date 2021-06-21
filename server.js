60431f58957dcc8d023fab2ca2024af26bb5d76b
Ross Keenan <ross.keenan@protonmail.com>
2021-06-21 14:47:20 -0500

    Added in CSS and js file. JS alert is to test the script is being recognized.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(express.static("static"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/templates/index.html"));
 });
